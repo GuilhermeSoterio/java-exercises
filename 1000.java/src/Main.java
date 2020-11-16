7d5b3fdf2ca59d33b38e26505fb08f0d07368bbc
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-11-16 11:23:25 -0300

    Primeiro exercício

---
+++ 1000.java/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	// Your first program in any programming language is usually "Hello World!".
+	//In this first problem all you have to do is print this message on the screen.
+		 
+	    public static void main(String[] args){
+	 
+	      System.out.println("Hello World!");
+	 
+	   }
+	 
+}
